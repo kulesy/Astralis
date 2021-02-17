48d6f9a0a3bc2be4ff1ce8b45745ea5987ce4456
kulesy <69125238+kulesy@users.noreply.github.com>
2021-02-17 22:28:10 +1300

    test

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ RED_SPACE_SHIP = pygame.image.load(os.path.join("assets", "pixel_ship_red_small.
 GREEN_SPACE_SHIP = pygame.image.load(os.path.join("assets", "pixel_ship_green_small.png"))
 BLUE_SPACE_SHIP = pygame.image.load(os.path.join("assets", "pixel_ship_blue_small.png"))
 
-#Player player
+#Player 
 YELLOW_SPACE_SHIP = pygame.image.load(os.path.join("assets", "pixel_ship_yellow.png"))
 
 # Lasers 
