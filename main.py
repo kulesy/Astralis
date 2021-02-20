f433820036f9cd5d51b46ee4fa00fb2a37c10e69
kulesy <kulewidak@gmail.com>
2021-02-20 13:56:26 +1300

    enemy movement

--- main.py
+++ main.py
@@ -55,8 +55,8 @@ class Player(Ship):
 
 class Enemy(Ship):
     COLOR_MAP = {
-                "red": (RED_SPACE_SHIP, RED_LASER)
-                "green": (GREEN_SPACE_SHIP, GREEN_LASER)
+                "red": (RED_SPACE_SHIP, RED_LASER),
+                "green": (GREEN_SPACE_SHIP, GREEN_LASER),
                 "blue": (BLUE_SPACE_SHIP, BLUE_LASER)
                 }
 
@@ -72,9 +72,13 @@ class Enemy(Ship):
 def main():
     run = True
     FPS = 60
-    level = 1
+    level = 0
     lives = 5
     main_font = pygame.font.SysFont("comicsans", 50)
+
+    enemies = []
+    wave_length = 5
+    enemy_vel = 1
 
     player_vel = 5
 
@@ -91,14 +95,22 @@ def main():
         WIN.blit(lives_label, (10, 10))
         WIN.blit(level_label, (WIDTH - level_label.get_width() - 10, 10))
         
+        for enemy in enemies:
+            enemy.draw(WIN)
+
         player.draw(WIN)
 
         pygame.display.update()
 
     while run:
         clock.tick(FPS)
-        redraw_window()
-
+        
+        if len(enemies) == 0: 
+            level += 1
+            wave_length += 5
+            for i in range(wave_length):
+                enemy = Enemy(random.randrange(50, WIDTH-100), random.randrange(-1500, -100), random.choice(["red", "blue", "green"]))
+                enemies.append(enemy)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
@@ -112,6 +124,10 @@ def main():
         if keys[pygame.K_s] and player.y + player_vel + player.get_height() < HEIGHT: # Down
             player.y += player_vel
         
-                
+        for enemy in enemies:
+            enemy.move(enemy_vel)
+
+        redraw_window()
+        
 main()        
 
