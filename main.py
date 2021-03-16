19a2463726703a26c1ae7483cc081c53d1b7d1dc
Elijah <kulewidak@gmail.com>
2021-03-17 10:59:51 +1300

    text for mainmenu

--- main.py
+++ main.py
@@ -37,8 +37,8 @@ LIVES.set_colorkey((BLACK))
 font = pygame.image.load(os.path.join('data', 'font', 'small_font.png'))
 
 # Cursor
-CURSOR = pygame.image.load(os.path.join('data', 'assets', 'cursor.png'))
-CURSOR.set_colorkey((BLACK))
+# CURSOR = pygame.image.load(os.path.join('data', 'assets', 'cursor.png'))
+# CURSOR.set_colorkey((BLACK))
 
 class Laser:
     def __init__(self, x, y, img):
@@ -201,8 +201,8 @@ class Game:
         self.level = 0
         self.hearts = 5
         self.lives = self.hearts
-        self.main_font = text.Font(os.path.join('data', 'font', 'small_font.png'), (WHITE))
-        self.lost_font = text.Font(os.path.join('data', 'font', 'large_font.png'), (WHITE))
+        self.small_font = text.Font(os.path.join('data', 'font', 'small_font.png'), (WHITE))
+        self.large_font = text.Font(os.path.join('data', 'font', 'large_font.png'), (WHITE))
 
         self.display = pygame.Surface((750, 750))
 
@@ -236,8 +236,8 @@ class Game:
         self.level = 0
         self.hearts = 5
         self.lives = self.hearts
-        self.main_font = text.Font(os.path.join('data', 'font', 'small_font.png'), (WHITE))
-        self.lost_font = text.Font(os.path.join('data', 'font', 'large_font.png'), (WHITE))
+        self.small_font = text.Font(os.path.join('data', 'font', 'small_font.png'), (WHITE))
+        self.large_font = text.Font(os.path.join('data', 'font', 'large_font.png'), (WHITE))
         self.display = pygame.Surface((750, 750))
         self.enemies = []
         self.wave_length = 5
@@ -271,8 +271,8 @@ class Game:
     def redraw_window(self):
         display.blit(BG, (0,0))
         # Draw text
-        level_label = self.main_font.render(f"Level: {self.level}", display, (WIDTH - self.main_font.width(f"Level: {self.level}") - 10, 10))
-        score_label = self.main_font.render(f"Score: {self.player.score}", display, (10, 10))
+        level_label = self.small_font.render(f"Level: {self.level}", display, (WIDTH - self.small_font.width(f"Level: {self.level}") - 10, 10))
+        score_label = self.small_font.render(f"Score: {self.player.score}", display, (10, 10))
         
         for enemy in self.enemies:
             enemy.draw(display)
@@ -282,10 +282,10 @@ class Game:
 
         if self.lost == True:
             if self.highscore > self.player.score:
-                lost_label = self.lost_font.render(f"Score: {self.player.score}", display, (WIDTH//2 - (self.lost_font.width(f"Score: {self.score}")//2), HEIGHT/2))
+                lost_label = self.large_font.render(f"Score: {self.player.score}", display, (WIDTH//2 - (self.large_font.width(f"Score: {self.score}")//2), HEIGHT/2))
             elif self.highscore <= self.player.score:
                 self.highscore = self.player.score
-                win_label = self.lost_font.render(f"New Highscore! : {self.highscore}", display, ((WIDTH//2 - 60), HEIGHT/2) )
+                win_label = self.large_font.render(f"New Highscore! : {self.highscore}", display, ((WIDTH//2 - 60), HEIGHT/2) )
                 with open(os.path.join(self.dir, HS_FILE), 'w') as f:
                     f.write(str(self.player.score))
         WIN.blit(pygame.transform.scale(display, (750,750)), (0,0))
@@ -380,7 +380,7 @@ class Game:
         text_surface = font.render(text, True, WHITE)
         text_rect = text_surface.get_rect()
         text_rect.center = (x,y)
-        self.display.blit(text_surface,text_rect)
+        display.blit(text_surface,text_rect)
     
     def update_lives(self):
         offset = ((LIVES.get_width() / 2) - 3)
@@ -390,16 +390,16 @@ class Game:
 
 class Menu:
     def __init__(self):
-        self.mid_w, self.mid_h = 750 / 2, 750 / 2
+        self.mid_w, self.mid_h = (WIDTH - game.large_font.width(f'Highscore: {str(game.highscore)}')) / 2, 200 / 2
         self.run_display = True
         self.cursor_rect = pygame.Rect(0, 0, 20, 20)
-        self.offset = - 100
+        self.offset = - 10
 
     def draw_cursor(self):
-        game.draw_text('*', 70, self.cursor_rect.x, self.cursor_rect.y)
+        game.draw_text('*', 20, self.cursor_rect.x, self.cursor_rect.y)
 
     def blit_screen(self):
-        WIN.blit((game.display), (0, 0))
+        WIN.blit(pygame.transform.scale((display), (750, 750)), (0,0))
         pygame.display.update()
         game.reset_keys()
 
@@ -407,9 +407,9 @@ class MainMenu(Menu):
     def __init__(self):
         Menu.__init__(self)
         self.state = "Start"
-        self.startx, self.starty = self.mid_w, self.mid_h + 30
-        self.optionsx, self.optionsy = self.mid_w, self.mid_h + 70
-        self.quitx, self.quity = self.mid_w, self.mid_h + 110
+        self.startx, self.starty = self.mid_w , self.mid_h + 10
+        self.optionsx, self.optionsy = self.mid_w, self.mid_h + 30
+        self.quitx, self.quity = self.mid_w , self.mid_h + 50
         self.cursor_rect.midtop = (self.startx + self.offset, self.starty + 10)
     
     def display_menu(self):
@@ -419,11 +419,11 @@ class MainMenu(Menu):
             game.check_events()
             self.check_input()
             game.display.fill(BLACK)
-            game.draw_text(f'Highscore: {str(game.highscore)}', 75, 750 / 2, 750 / 2 - 250)
-            game.draw_text('Main Menu', 100, 750 / 2, 750 / 2 - 100)
-            game.draw_text('Start Game', 50, self.startx, self.starty)
-            game.draw_text('Options', 50, self.optionsx, self.optionsy)
-            game.draw_text('Quit', 50, self.quitx, self.quity)
+            game.large_font.render(f'Highscore: {str(game.highscore)}', display, ((WIDTH - game.large_font.width(f'Highscore: {str(game.highscore)}')) / 2, HEIGHT / 2 - 25))
+            game.large_font.render('Main Menu', display, ((WIDTH - game.large_font.width('Main Menu')) / 2, HEIGHT / 2 - 10))
+            game.large_font.render('Start Game', display, ((WIDTH - game.large_font.width('Start Game')) / 2, self.starty))
+            game.large_font.render('Options', display, ((WIDTH - game.large_font.width('Options')) / 2, self.optionsy))
+            game.large_font.render('Quit', display, ((WIDTH - game.large_font.width('Quit')) / 2, self.quity))
             self.draw_cursor()
             self.blit_screen()
 
